a52cc0ee906a12672e17636fa236a1235e0924d7
kuldeep.singh <kuldeep.s@medibuddy.in>
2025-09-07 19:11:41 +0530

    delete the core_test.clj

--- test/math_quiz_server/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns math-quiz-server.core-test
-  (:require [clojure.test :refer :all]
-            [math-quiz-server.core :refer :all]))
-
-(deftest a-test
-  (testing "Simple Test which passes."
-    (is (= 1 1))))
