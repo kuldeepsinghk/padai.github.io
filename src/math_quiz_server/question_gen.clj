be0a7d9e106f77b0f3be53ed255864f51422ce1c
kuldeep.singh <kuldeep.s@medibuddy.in>
2025-09-13 22:43:05 +0530

    get topics for a grade subject combo

--- src/math_quiz_server/question_gen.clj
+++ src/math_quiz_server/question_gen.clj
@@ -29,6 +29,35 @@
 (def grade-7 (grade :grade-7))
 (def grade-10 (grade :grade-10))
 
+;; Define Subject as an enum with keyword values
+(def Subject
+  {:keys #{:subject-Math :subject-Science :subject-Physics :subject-Chemistry :subject-Biology}
+   :valid? (fn [value] (contains? #{:subject-Math :subject-Science :subject-Physics 
+                                   :subject-Chemistry :subject-Biology} value))
+   :to-string (fn [value] 
+                (when ((:valid? Subject) value)
+                  (subs (name value) 8)))})
+
+;; Define a record type for Subject
+(defrecord SubjectType [value])
+
+;; Smart constructor that ensures only valid Subject values can be created
+(defn subject
+  "Creates a Subject instance with validation. Throws IllegalArgumentException if invalid."
+  [key-value]
+  (if ((:valid? Subject) key-value)
+    (->SubjectType key-value)
+    (throw (IllegalArgumentException. 
+             (str "Invalid subject: " key-value 
+                  ". Must be one of: " (clojure.string/join ", " (:keys Subject)))))))
+
+;; Convenience helper constants for common subjects
+(def subject-Math (subject :subject-Math))
+(def subject-Science (subject :subject-Science)) 
+(def subject-Physics (subject :subject-Physics))
+(def subject-Chemistry (subject :subject-Chemistry))
+(def subject-Biology (subject :subject-Biology))
+
 ;; Define schema for grade-subjects.json structure
 (def topic-schema 
   [:map
@@ -37,6 +66,10 @@
 ;; Define schema for Grade validation
 (def grade-schema 
   [:enum :grade-6 :grade-7 :grade-10])
+
+;; Define schema for Subject validation
+(def subject-schema
+  [:enum :subject-Math :subject-Science :subject-Physics :subject-Chemistry :subject-Biology])
 
 (def grade-subjects-schema
   [:map-of 
@@ -72,3 +105,36 @@
               (println "Validation error:" explain-result)
               []))))
       nil)))
+
+(defn get-topics-for-subject
+  "Given a Grade instance and a Subject instance, returns a list of topics for that subject.
+   This function requires proper Grade and Subject instances created with their respective constructors."
+  [^GradeType grade-instance ^SubjectType subject-instance]
+  (let [grade-key (:value grade-instance)
+        subject-key (:value subject-instance)
+        grade-str ((:to-string Grade) grade-key)
+        subject-str ((:to-string Subject) subject-key)
+        resource (io/resource "public/grade-subjects.json")]
+    (println "Looking up topics for subject" subject-str "in grade" grade-str)
+    (if resource
+      (let [content (slurp resource)
+            parsed-data (json/parse-string content true)]
+        ;; Validate data structure
+        (if (m/validate grade-subjects-schema parsed-data)
+          (let [grade-data (get parsed-data (keyword grade-str))
+                subject-data (get grade-data (keyword subject-str))]
+            (if subject-data
+              ;; Return topic names as keywords
+              (vec (keys subject-data)) 
+              ;; Subject not found in this grade
+              (do 
+                (println "Subject" subject-str "not found in grade" grade-str)
+                [])))
+          ;; Invalid JSON structure
+          (do
+            (println "Warning: JSON data does not match schema")
+            [])))
+      ;; Resource not found
+      (do
+        (println "Resource not found")
+        nil))))
--- test/math_quiz_server/question_gen_test.clj
+++ test/math_quiz_server/question_gen_test.clj
@@ -28,4 +28,20 @@
       
       (is (not (nil? subjects)) "Subjects should not be nil")
       (is (vector? subjects) "Subjects should be a vector")
-      (is (pos? (count subjects)) "Should have at least one subject"))))
+      (is (pos? (count subjects)) "Should have at least one subject")
+      (is (some #{:Math} subjects) "Should include Math subject")
+      (is (some #{:Science} subjects) "Should include Science subject"))))
+
+(deftest test-get-topics-for-subject
+  (testing "Getting topics for a specific subject in a grade"
+    (println "\n--- BEGIN TEST DEBUG INFO ---")
+    (println "Running test for Math topics in Grade 6")
+    
+    (let [topics (gen/get-topics-for-subject gen/grade-6 gen/subject-Math)]
+      (println "Topics returned:" topics)
+      (println "--- END TEST DEBUG INFO ---\n")
+      
+      (is (not (nil? topics)) "Topics should not be nil")
+      (is (vector? topics) "Topics should be a vector")
+      (is (pos? (count topics)) "Should have at least one topic")
+      (is (some #(= (name %) "Basic Arithmetic") (map name topics)) "Should include Basic Arithmetic as a topic"))))
